508ee0557a8fb7c848d9f580e676b010ddc93afa
Axel Forsman <axel@axelf.se>
2023-08-15 09:42:50 +0200

    Read from buffer

--- derl.el
+++ derl.el
@@ -1,22 +1,11 @@
-;;; derl.el --- Erlang distribution protocol implementation -*- lexical-binding: t -*-
+;;; derl.el --- Erlang distribution protocol implementation  -*- lexical-binding: t -*-
 
 ;; Start with "erl -sname arnie" and get cookie
 
 (eval-when-compile (require 'cl-lib))
+(require 'generator)
 
-(defmacro derl--read-uint (n string offset)
-  "Read N-byte network-endian unsigned integer from STRING at OFFSET."
-  (macroexp-let2* nil ((s string) (j offset))
-    (cl-loop
-     for i below n collect
-     (let ((x `(aref ,string ,(cl-case i
-                                (0 j)
-                                (1 `(1+ ,j))
-                                (t `(+ ,j ,i))))))
-       (if (< i (1- n)) `(ash ,x ,(* 8 (- n i 1))) x))
-     into xs finally return `(logior ,@xs))))
-
-(defmacro derl--read-uint* (n &optional position)
+(defmacro derl--read-uint (n &optional position)
   "Read N-byte network-endian unsigned integer."
   (cl-loop
    for i below n collect
@@ -59,7 +48,7 @@
                 (let ((result (aref string 1)))
                   (funcall
                    cb (if (/= result 0) (cons 'error result)
-                        (derl--read-uint 2 string 2)))))) ; portno
+                        (logior (ash (aref string 2) 8) (aref string 3))))))) ; portno
             :sentinel (lambda (proc event) (funcall cb (cons 'error event)))
             :plist (list 'buf ""))))
       (process-send-string
@@ -205,10 +194,10 @@
 
 (cl-defstruct (derl-process (:type vector) (:constructor nil)
                             (:copier nil) (:predicate nil))
-  (id (:read-only t)) (cond-var (:read-only t)) mailbox thread)
+  (id (:read-only t)) function mailbox blocked)
 
 ;; Process-local variables
-(defvar derl--self (vector 0 (make-condition-variable (make-mutex)) () main-thread)
+(defvar derl--self [0 nil () nil]
   "The current `derl-process'.")
 (defvar derl--mailbox ()
   "The local mailbox of the current process.")
@@ -220,46 +209,65 @@
   "Map of PID:s to processes.")
 (defvar derl--next-pid 0)
 
+(defvar derl--scheduler-timer nil)
+
+(defun derl--schedule-scheduler ()
+  (unless derl--scheduler-timer
+    (setq derl--scheduler-timer (run-with-timer 0 nil #'derl--scheduler-run))))
+
+(defun derl--scheduler-run ()
+  (unless (= (derl-process-id derl--self) 0) (error "Scheduling from inferior process"))
+  (message "Running scheduler")
+  (setq derl--scheduler-timer nil)
+  (while (let* ((schedulable (cl-loop for p being the hash-values of derl--processes
+                                      unless (derl-process-blocked p) collect p))
+                (derl--self (nth (random (length schedulable)) schedulable)))
+           (cond
+            ;; Main process is blocked on externalities
+            ((null derl--self) (accept-process-output))
+            ;; Pass control back to main process
+            ((= (derl-process-id derl--self) 0)
+             (when (cdr schedulable) (derl--schedule-scheduler))
+             nil)
+            (t (ignore-error iter-end-of-sequence
+                 (iter-next (derl-process-function derl--self)))
+               t)))))
+
 (defun derl-spawn (fun)
   (let* ((id (cl-incf derl--next-pid))
-         (mutex (make-mutex))
-         (cond-var (make-condition-variable mutex))
-         (process (vector id cond-var () nil))
-         (thread
-          (make-thread
-           (lambda ()
-             (let ((derl--self process) derl--mailbox)
-               (unwind-protect (funcall fun)
-                 (remhash (derl-process-id derl--self) derl--processes)))))))
-    (setf (derl-process-thread process) thread)
+         (fun* (iter-make (unwind-protect (iter-yield-from fun)
+                            (remhash id derl--processes))))
+         (process (vector id fun* () nil)))
     (puthash id process derl--processes)
+    (derl--schedule-scheduler)
     id))
 
-(defmacro derl-receive (&rest arms)
+(cl-defmacro derl-receive
+    (&body arms &aux (catchallp (cl-loop for (x) in arms thereis (symbolp x)))
+           &environment env)
   `(cl-loop
-    with fetch =
-    (lambda ()
-      (with-mutex (condition-mutex (derl-process-cond-var derl--self))
-        (while (null (derl-process-mailbox derl--self))
-          (condition-wait (derl-process-cond-var derl--self)))
-        (prog1 (nreverse (derl-process-mailbox derl--self))
-          (setf (derl-process-mailbox derl--self) ()))))
-    for cell = (or derl--mailbox (setq derl--mailbox (funcall fetch))) then
-    (or (cdr cell)
-        (let ((xs (funcall fetch)))
-          (if cell (setcdr cell xs) (setq derl--mailbox xs))))
+    for cell =
+    (or (if cell (cdr cell) derl--mailbox)
+        (cl-letf (((derl-process-blocked derl--self) t))
+          (while (null (derl-process-mailbox derl--self))
+            ,(if (assq 'iter-yield env) '(iter-yield nil) '(derl--scheduler-run)))
+          (let ((xs (nreverse (derl-process-mailbox derl--self))))
+            (setf (derl-process-mailbox derl--self) ())
+            (if cell (setcdr cell xs) (setq derl--mailbox xs)))))
     and prev = cell with result while
-    (let (continue)
-      (setq result (pcase (car cell) ,@arms (_ (setq continue t))))
+    (let ((continue nil))
+      (setq result (pcase (car cell) ,@arms ,@(unless catchallp '((_ (setq continue t))))))
       continue)
     finally (if prev (setcdr prev (cdr cell)) (pop derl--mailbox))
     finally return result))
 
 (defun derl-send (pid expr)
   (when-let (process (gethash pid derl--processes))
-    (with-mutex (condition-mutex (derl-process-cond-var process))
-      (push expr (derl-process-mailbox process))
-      (condition-notify (derl-process-cond-var process)))))
+    (push expr (derl-process-mailbox process))
+    (derl--schedule-scheduler)
+    (setf (derl-process-blocked process) nil)))
+
+(derl-send (derl-spawn (iter-make (message "ran: %S" (derl-receive (x x))))) 545)
 
 ;;; Erlang Distribution Protocol
 
@@ -269,83 +277,85 @@
 
 (cl-defun derl-connect (host port cookie)
   (cl-labels
-      ((get-msg (proc string lenlen)
+      ((recv-status (proc)
+         (unless (eq (get-byte) ?s) (error "Bad status"))
+         (forward-char)
+         (cond
+          ((looking-at-p "named:")
+           (forward-char 6)
+           (let* ((nlen (derl--read-uint 2))
+                  (name (progn (forward-char nlen)
+                               (buffer-substring-no-properties (- (point) nlen) (point))))
+                  (creation (derl--read-uint 4)))
+             (message "Name: %s, creation: %s" name creation)
+             (process-put proc 'name (intern name))
+             (process-put proc 'creation creation)
+             (process-put proc 'filter #'recv-challenge)))
+          ;; TODO alive and case 3B)
+          (t (error "Unknown status"))))
+       (recv-challenge (proc)
+         (cl-assert (eq (get-byte) ?N)) ; recv_challenge_reply tag
+         (forward-char 9)
+         (let* ((challenge-b (derl--read-uint 4))
+                (creation-b (derl--read-uint 4))
+                (nlen (derl--read-uint 2))
+                (name-b (progn (forward-char nlen)
+                               (buffer-substring-no-properties (- (point) nlen) (point))))
+                (challenge-a (random (ash 1 32))) ; #x100000000
+                (digest (derl--gen-digest challenge-b cookie)))
+           (process-put proc 'name-b (intern name-b))
+           (process-put proc 'creation-b creation-b)
+           (process-send-string
+            proc (concat [0 21 ; Length
+                            ?r] ; send_challenge_reply tag
+                         (derl--write-uint 4 challenge-a)
+                         digest))
+           (process-put proc 'filter #'recv-challenge-ack)))
+       (recv-challenge-ack (proc)
+         (unless (eq (get-byte) ?a) (error "Bad tag"))
+         (let ((digest (buffer-substring-no-properties (1+ (point)) (+ (point) 1 16))))
+           (forward-char 17)
+           (message "Got challenge ACK: digest %S!" digest)
+           (process-put proc 'filter #'connected)))
+       (connected (proc)
+         (if (= (- (point-max) (point-min)) 0)
+             (process-send-string proc "\0\0\0\0") ; Zero-length heartbeat
+           (cl-assert (eq (get-byte) 112)) ; Check that type is pass through
+           (forward-char)
+           (let ((control-msg
+                  (progn (cl-assert (eq (get-byte) derl-ext-version))
+                         (forward-char)
+                         (derl-read)))
+                 (msg (progn (cl-assert (eq (get-byte) derl-ext-version))
+                             (forward-char)
+                             (derl-read))))
+             (message "Parsed: %S" (cons control-msg msg))
+             (pcase control-msg
+               ;; SEND_SENDER
+               (`[22 ,_from-pid [,_ pid ,_name ,to-pid ,_serial ,_creation]]
+                (derl-send to-pid msg))))))
+       (filter (proc string)
          (with-current-buffer (process-buffer proc)
            (insert string)
-           (let (len)
-             (when (and (<= lenlen (buffer-size))
-                        (<= (setq len (+ lenlen (if (= lenlen 4)
-                                                    (derl--read-uint* 4 (point-min))
-                                                  (derl--read-uint* 2 (point-min)))))
-                            (buffer-size)))
-               (prog1 (buffer-substring-no-properties
-                       (+ (point-min) lenlen) (+ (point-min) len))
-                 (delete-region (point-min) (+ (point-min) len)))))))
-       (recv-status (proc string)
-         (when-let (s (get-msg proc string 2))
-           (unless (eq (aref s 0) ?s) (error "Bad status"))
-           (funcall
-            (cond
-             ((eq (compare-strings s 1 7 "named:" nil nil) t)
-              (let* ((nlen (derl--read-uint 2 s 7))
-                     (name (substring s 9 (+ 9 nlen)))
-                     (creation (derl--read-uint 4 s (+ 9 nlen))))
-                (message "Name: %s, creation: %s" name creation)
-                (process-put proc 'name (intern name))
-                (process-put proc 'creation creation)
-                (set-process-filter proc #'recv-challenge)))
-             ;; TODO alive and case 3B)
-             (t (error "Unknown status: %S" s)))
-            proc "")))
-       (recv-challenge (proc string)
-         (when-let (s (get-msg proc string 2))
-           (cl-assert (eq (aref s 0) ?N)) ; recv_challenge_reply tag
-           (let* ((challenge-b (derl--read-uint 4 s 9))
-                  (creation-b (derl--read-uint 4 s 13))
-                  (nlen (derl--read-uint 2 s 17))
-                  (name-b (substring s 19 (+ 19 nlen)))
-                  (challenge-a (random (ash 1 32))) ; #x100000000
-                  (digest (derl--gen-digest challenge-b cookie)))
-             (process-put proc 'name-b (intern name-b))
-             (process-put proc 'creation-b creation-b)
-             (process-send-string
-              proc (concat [0 21 ; Length
-                              ?r] ; send_challenge_reply tag
-                           (derl--write-uint 4 challenge-a)
-                           digest))
-             (set-process-filter proc #'recv-challenge-ack))))
-       (recv-challenge-ack (proc string)
-         (unless (eq (aref string 2) ?a) (error "Bad tag"))
-         (let ((digest (substring string 3 (+ 3 16))))
-           (message "Got challenge ACK: digest %S!" digest)
-           (set-process-filter proc #'connected-filter)))
-       (connected-filter (proc string)
-         (when-let (s (get-msg proc string 4))
-           (if (string= s "")
-               (process-send-string proc "\0\0\0\0") ; Zero-length heartbeat
-             (cl-assert (eq (aref s 0) 112)) ; Check that type is pass through
-             (with-temp-buffer
-               (set-buffer-multibyte nil)
-               (insert s)
-               (goto-char (1+ (point-min)))
-               (let ((control-msg
-                      (progn (cl-assert (eq (char-after) derl-ext-version))
-                             (forward-char)
-                             (derl-read)))
-                     (msg (progn (cl-assert (eq (char-after) derl-ext-version))
-                                 (forward-char)
-                                 (derl-read))))
-                 (message "Parsed: %S" (cons control-msg msg))
-                 (pcase control-msg
-                   ;; SEND_SENDER
-                   (`[22 ,_from-pid [,_ pid ,_name ,to-pid ,_serial ,_creation]]
-                    (derl-send to-pid msg))))))
-           (connected-filter proc ""))))
+           (goto-char (point-min))
+           (let ((lenlen (if (eq (process-get proc 'filter) #'connected) 4 2))
+                 len)
+             (while (and (<= lenlen (buffer-size))
+                         (<= (setq len (+ lenlen (if (= lenlen 4) (derl--read-uint 4)
+                                                   (derl--read-uint 2))))
+                             (buffer-size)))
+               (let ((f (process-get proc 'filter)))
+                 (save-restriction
+                   (narrow-to-region (point) (+ (point-min) len))
+                   (funcall f proc)
+                   (unless (= (point) (point-max)) (error "Bad length")))
+                 (delete-region (point-min) (+ (point-min) len)))))
+           (goto-char (point-max)))))
     (let* ((buf (generate-new-buffer " *erl recv*" t))
            (proc (make-network-process
                   :name "erl-emacs" :buffer buf :host host :service port
-                  :coding 'raw-text :filter-multibyte nil :filter #'recv-status
-                  :plist ()))
+                  :coding 'raw-text :filter-multibyte nil :filter #'filter
+                  :plist (list 'filter #'recv-status)))
            (flags
             (eval-when-compile
               (derl--write-uint
