20a4a1fa3a31b501ac078fa17247ef217fff90ab
Axel Forsman <axel@axelf.se>
2023-09-21 11:41:46 +0200

    Add license notice to package header

--- derl.el
+++ derl.el
@@ -1,18 +1,36 @@
 ;;; derl.el --- Erlang distribution protocol implementation  -*- lexical-binding: t -*-
 
+;; Copyright (C) Axel Forsman
+
 ;; Author: Axel Forsman <axel@axelf.se>
-;; Keywords: comm, extensions, languages, processes
+;; Maintainer: Axel Forsman <axel@axelf.se>
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "29.1"))
+;; Keywords: comm, extensions, languages, processes
+;; URL: https://github.com/axelf4/derl.el
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
 ;; This library implements the Erlang distribution protocol as
 ;; described in
 ;; https://www.erlang.org/doc/apps/erts/erl_dist_protocol.html,
-;; allowing Emacs to communicate with Erlang VM:s as if it were
-;; another Erlang node. This is achieved with an Erlang-like process
-;; runtime.
+;; allowing Emacs to communicate with Erlang VMs as if it were another
+;; Erlang node. This is achieved with an Erlang-like process runtime.
 
 ;; New processes are created by providing `derl-spawn' with a
 ;; generator function (see the `generator' package) to run. Scheduling
